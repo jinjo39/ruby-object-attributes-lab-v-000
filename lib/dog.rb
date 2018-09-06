6cc8c0a047f3fadad551afcd4cbd2d8bacb6b8ca
Christina Delavar <c.delavar@gmail.com>
2018-09-06 22:04:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,4 +10,16 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
+
+  def inititalize(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed - new_breed
+  end
 end
